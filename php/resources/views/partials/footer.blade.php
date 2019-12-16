4217adb96de4e53c9b2e6df6342ba12b301313b1
Paul Ho <paulho@Pauls-IMAC.local>
2019-12-16 20:44:51 +0800

    updated

--- php/resources/views/partials/footer.blade.php
+++ php/resources/views/partials/footer.blade.php
@@ -1,11 +1,8 @@
- <!--加入酷力量-->
+ <!--加入-->
  <section class="joinQuelea">
     <div class="joinQueleaBox">
-        <h3>Quelea 酷力量 線上健身房</h3>
-        <p>『健身房即在家』讓使用者可以在家就有上百堂不段更新的健身菜單，更邀請營養師、專科醫師提供養生建議。不設限時間、空間，任何時候皆能觀看線上課程，更能與家人共享。</p>
-        <a class="yellowButton" href="https://www.queleagym.com/join_sn.php">
-            <p>加入會員</p>
-        </a>
+        <h3>線上購物</h3>
+        <p></p>
     </div>
 </section>
 
@@ -16,21 +13,20 @@
             <img class="img-responsive" src="{{ asset('images/index-bottom-logo-150.png') }}" alt="">
         </div>
         <div class="footerItem">
-            <h4>Quelea線上購物</h4>
+            <h4>線上購物</h4>
             <ul>
-                <li class="halfTheLi"><a href='{{ route('category.list', 'machines') }}'>健身器材</a></li>
-                <li class="halfTheLi"><a href='{{ route('category.list', 'gadgets') }}'>健身小物</a></li>
+                <li class="halfTheLi"><a href='{{ route('category.list', 'machines') }}'>器材</a></li>
+                <li class="halfTheLi"><a href='{{ route('category.list', 'gadgets') }}'>小物</a></li>
                 <li class="halfTheLi"><a href='{{ route('category.list', 'accessories') }}'>器材配件</a></li>
                 <li class="halfTheLi"><a href='{{ route('cs.faqs') }}'>常見問題</a></li>
                 <li class="halfTheLi"><a href='{{ route('cs.contact') }}'>聯絡我們</a></li>
-                <li class="halfTheLi"><a href='https://www.queleagym.com'>線上健身房</a></li>
             </ul>
         </div>
         <div class="footerItem">
             <h4>聯絡我們</h4>
             <ul>
-                <li>客服電話：<a href="tel:0800-665-818">0800-665818</a></li>
-                <li>客服信箱：<a href="##">bhinfo@bhasia.com.tw</a></li>
+                <li>客服電話：<a href="tel:0800-665-818">0800-000987</a></li>
+                <li>客服信箱：<a href="##">info@eshop.com.tw</a></li>
             </ul>
         </div>
         <div class="footerItem">
@@ -42,7 +38,7 @@
         </div>
     </div>
     <div class="footerBottom">
-        <p>Copyright © 2019 Quelea酷力量線上購物 All rights reserved.</p>
+        <p>Copyright © 2019 線上購物 All rights reserved.</p>
     </div>
 </section>
 
--- php/resources/views/partials/nav.blade.php
+++ php/resources/views/partials/nav.blade.php
@@ -1,12 +1,11 @@
 <div class="queleaTopListMenu">
     <div class="container">
         <ul class="queleaListMenu list-inline">
-            <li><a href='{{ route('category.list', 'machines') }}'>健身器材</a></li>
-            <li><a href='{{ route('category.list', 'gadgets') }}'>健身小物</a></li>
+            <li><a href='{{ route('category.list', 'machines') }}'>器材</a></li>
+            <li><a href='{{ route('category.list', 'gadgets') }}'>小物</a></li>
             <li><a href='{{ route('category.list', 'accessories') }}'>器材配件</a></li>
             <li><a href='{{ route('cs.faqs') }}'>常見問題</a></li>
             <li><a href='{{ route('cs.contact') }}'>聯絡我們</a></li>
-            <li><a href='https://www.queleagym.com'>線上健身房</a></li>
         </ul>
     </div>
 </div>
